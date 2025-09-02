631d6dfc4bde35da61efad0ee778f85913580994
Marcel <marcel97.ganske@gmail.com>
2025-09-02 21:00:29 +0200

    Program play Game Function

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 
-/*
-Schreibe eine Funktion für die Auswahl des Computers
-Die Funktion soll entweder "Rock", "Paper" oder "Scissor" zurückgeben
-Test die Funktion in der Console 
-*/
-
 function getComputerChoice(){
 
     let randomNumber = 0;
@@ -23,25 +17,71 @@ function getComputerChoice(){
     return ""
 }
 
-console.log(getComputerChoice())
-
-/*
-Die Funktion soll die Auswahl des menschlichen Spielers zurückgeben 
-
-*/
 
 function getHumanChoice() {
     let humanChoice = "";
-    const readline = require("readline");
-    let rl = readline.createInterface(process.stdin, process.stdout);
-        rl.question("What is your Choice ?: ", (humanInput) => {
-            humanChoice = humanInput
-            return
-        }
-    )
+    humanChoice = prompt("What is your Choice ?: ");
     return humanChoice;
 }
 
-getHumanChoice();
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
 
-return
+    console.log("Human choice: " + humanChoice)
+    console.log("Computer choice: " + computerChoice)
+
+    if (humanChoice == computerChoice){
+        console.log("No Winner this Time")
+        return "none"
+    }
+    else if(
+        (humanChoice == "rock" && computerChoice == "scissor") ||
+        (humanChoice == "scissor" && computerChoice == "paper") ||
+        (humanChoice == "paper" && computerChoice == "rock")
+    ){
+        console.log("Human wins")
+        return "human"
+
+    } 
+    else if (
+        (computerChoice == "rock" && humanChoice == "scissor") ||
+        (computerChoice == "scissor" && humanChoice == "paper") ||
+        (computerChoice == "paper" && humanChoice == "rock")
+    )
+    
+    {
+        console.log("Computer wins")
+        return "computer"
+    }
+
+}
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for(let i = 0; humanScore < 5; i++){
+        let winner = playRound(getHumanChoice(), getComputerChoice())
+        
+        if (winner == "human"){
+            humanScore++
+        } 
+        else if (winner == "computer"){
+            computerScore++
+        }
+        console.log("Human Score: " + humanScore)
+        console.log("Computer Score " + computerScore)
+    }
+
+    if(humanScore > computerScore){
+        console.log("You are the Winner")
+    }
+    else if(humanScore < computerScore){
+        console.log("You lost, my dear Friend !!")
+    } else {
+        console.log("No Winner, No Chicken Dinner")
+    }
+}
+
+playGame()
